bade42bbb1f6a9952b58408e04721432660eca9f
NatWilleme <nathanwilleme@gmail.com>
2021-09-07 22:28:23 +0200

    finition de la vue search

--- controllers/controllersFinal/controller_search.php
+++ controllers/controllersFinal/controller_search.php
@@ -1,8 +1,11 @@
 <?php
 
+
+
 function getSearchResult()
 {
     $searchResult = companiesManager::searchCompany($_GET['company']);
+    $userAddress = getUserAddress();
     foreach ($searchResult as $company) {
         $rate = commentsManager::getRatingForCompany($company->id);
         $company->__set('rating', $rate['rate']); 
@@ -16,7 +19,24 @@ function getSearchResult()
         $domainesAsString = substr($domainesAsString,0,-2);
         $domainesAsString .= '.';
         $company->__set('domaines', $domainesAsString); 
+        
+        $companyAddress = $company->number.' '.$company->street.', '.$company->postalCode.' '.$company->city;
+        $company->distance = getDistance($userAddress, $companyAddress, $unit = 'K');
+
+        $rate = commentsManager::getRatingForCompany($company->id);
+        $company->__set('rating', $rate['rate']); 
+
+        $domaines = categoriesManager::getAllDomainesForCompany($company->id);
+        $domainesAsString = "";
+        foreach ($domaines as $domaine) {
+            $domainesAsString .= $domaine;
+            $domainesAsString .= ", ";
+        }
+        $domainesAsString = substr($domainesAsString,0,-2);
+        $domainesAsString .= '.';
+        $company->__set('domaines', $domainesAsString);
     } 
+    usort($searchResult, 'cmp');
     return $searchResult;
 }
 
--- views/viewFinal/view_search.php
+++ views/viewFinal/view_search.php
@@ -3,8 +3,8 @@
     ob_start();	
 ?>
 
-<div class="container-fluid ps-0 pe-0 mt-2 mb-5">
-    <h2>Résultat de la recherche:</h2>
+<div class="container-fluid mt-2 mb-5">
+    <h2 class="fw-bold text-center mb-3">Résultat de la recherche:</h2>
     <div class="row d-flex justify-content-around mb-4">
     <?php
         if(sizeof($searchResult) == 0){
@@ -12,35 +12,30 @@
         } else {
             $cpt = 0;
             foreach ($searchResult as $company) {
-                $cpt++;
-                if ($cpt > 5) {
-                    echo "</div><div class='row d-flex justify-content-around mb-4'>";
-                    $cpt = 1;
-                }
             ?>
             
-        <div class="card" style="width: 18rem;">
-            <img src="<?php echo $company->image; ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"><b><?php echo $company->name; ?></b></h5>
-                <p class="card-text">
-                    <?php 
-                        if ($company->rating == 0) {
-                            echo "<b>Pas encore de note !</b>";
-                        } 
-                        else echo "<b>Note :</b> ".number_format($company->rating,1)." <i class='bi bi-star-fill text-warning'></i>";
-                    ?>
-                    <br>
-                    <b>Domaine(s) :</b> 
-                    <?php
-                        echo $company->domaines;
-                    ?>
-                    <br>
-                    <b>Localisation :</b> <?php echo $company->city; ?>
-                </p>
-                <a href="index.php?viewToDisplay=displayCompanyDetails&idCompany=<?php echo $company->id; ?>" class="btn btn-primary">Accéder</a>
-            </div>
-        </div>          
+            <div class="card border border-dark pt-2 ms-3 me-3 mb-5" style="width: 18rem;">
+                <img src="<?php echo $company->image; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><b><?php echo $company->name; ?></b></h5>
+                    <p class="card-text">
+                        <?php 
+                            if ($company->rating == 0) {
+                                echo "<b>Pas encore de note !</b>";
+                            } 
+                            else echo "<b>Note :</b> ".number_format($company->rating,1)." <i class='bi bi-star-fill text-warning'></i>";
+                        ?>
+                        <br>
+                        <b>Domaine(s) :</b> 
+                        <?php
+                            echo $company->domaines;
+                        ?>
+                        <br>
+                        <b>Distance :</b> <?php echo $company->distance; ?> km
+                    </p>
+                </div>
+                <a href="index.php?viewToDisplay=displayCompanyDetails&idCompany=<?php echo $company->id; ?>" class="btn btn-primary mb-2">Accéder</a>
+            </div>          
 
     <?php
             }
