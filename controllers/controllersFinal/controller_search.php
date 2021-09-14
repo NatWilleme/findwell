530b17eb573c22a8d276dece24f59dda30407aa4
NatWilleme <nathanwilleme@gmail.com>
2021-09-14 16:40:28 +0200

    test location

--- controllers/controllersFinal/controller_search.php
+++ controllers/controllersFinal/controller_search.php
@@ -5,6 +5,7 @@
 function getSearchResult()
 {
     $searchResult = companiesManager::searchCompany($_POST['company']);
+    print_r($_POST);
     if(!isset($_POST['location']) || $_POST['location'] == ""){
         $userAddress = null;
     } else {
